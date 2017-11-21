9201d46e374df0fc9e79bf263c4f07316ceea8de
Joao Campos <j.campos4g@gmail.com>
2017-11-21 15:10:00 -0500

    added about and team html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,28 @@ import { Routes, RouterModule, RouterLink } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { LabmonComponent } from './labmon/labmon.component';
+import { TeamComponent } from './labmon/team/team.component';
+import { LinksComponent } from './labmon/links/links.component';
+import { VideosComponent } from './labmon/videos/videos.component';
+import { HomeComponent } from './home/home.component';
 
 const myRoutes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'labmon/about',     component: LabmonComponent },
+  { path: 'labmon/team',     component: TeamComponent },
+  { path: 'labmon/links',     component: LinksComponent },
+  { path: 'labmon/videos',     component: VideosComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    LabmonComponent
+    LabmonComponent,
+    TeamComponent,
+    LinksComponent,
+    VideosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
